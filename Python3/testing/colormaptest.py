5be8e45b971f76cb11c89c153def4b8dea80597b
Kris Hauser <kris.hauser@duke.edu>
2020-10-04 03:10:59 -0400

    Testing lighting

--- Python3/testing/colormaptest.py
+++ Python3/testing/colormaptest.py
@@ -19,35 +19,43 @@ w = WorldModel()
 w.makeRigidObject("a")
 w.rigidObject(0).geometry().set(a)
 
+#a_pc = a.convert("PointCloud",0.05)
+bb = a.getBBTight()
 a_pc = a.convert("PointCloud")
-a_pc.setCurrentTransform(so3.identity(),[1,0,0])
+a_pc.setCurrentTransform(so3.identity(),[1.25*(bb[1][0]-bb[0][0]),0,0])
 value = None
 cmap = None
 feature = None
+lighting = None
 
 vis.add("A",a)
 vis.add("B",a_pc)
 
-def convert(value_new,cmap_new,feature_new=None):
-    global value,cmap,feature,w
+def convert(value_new,cmap_new,feature_new=None,lighting_new=None):
+    global value,cmap,feature,lighting,w
     if value_new is None or value_new == value:
         if cmap_new is None or cmap == cmap_new:
             if feature_new is None or feature == feature_new:
-                return
+                if lighting_new is None or feature == lighting_new:
+                    return
     if cmap_new is not None:
         cmap = cmap_new
     if value_new is not None:
         value = value_new
     if feature_new is not None:
         feature = feature_new
+    if lighting_new is not None:
+        lighting = lighting_new
+        if lighting_new == 'none':
+            lighting = None
     if value is None:
         value = 'z'
     if cmap is None:
         cmap = 'viridis'
-    a_app = colorize.colorize(w.rigidObject(0),value,cmap,feature)
+    a_app = colorize.colorize(w.rigidObject(0),value,cmap,feature,lighting=lighting)
     #a_app.drawGL(a)
-    if not value.startswith('n'):
-        colorize.colorize(a_pc,value,cmap)
+    #if not value.startswith('n'):
+    colorize.colorize(a_pc,value,cmap,lighting=lighting)
     vis.remove("A")
     vis.add("A",a_app)
     vis.remove("B")
@@ -70,6 +78,8 @@ vis.addAction(lambda:convert('ny',None),"ny value")
 vis.addAction(lambda:convert('nz',None),"nz value")
 vis.addAction(lambda:convert(None,None,'faces'),"colorize faces")
 vis.addAction(lambda:convert(None,None,'vertices'),"colorize vertices")
+vis.addAction(lambda:convert(None,None,None,[0,0,-1]),"lighting on")
+vis.addAction(lambda:convert(None,None,None,'none'),"lighting off")
 
 
 vis.show()
