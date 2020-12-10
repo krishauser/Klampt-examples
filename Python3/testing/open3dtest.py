d1122c88d70c2b89cb895d6ac049f06e1d1c266d
Kris Hauser <kris.hauser@duke.edu>
2020-12-09 22:01:20 -0500

    Updated 0.8+ version of open3d

--- Python3/testing/open3dtest.py
+++ Python3/testing/open3dtest.py
@@ -3,7 +3,7 @@ from klampt.io import open3d_convert
 from klampt import vis
 
 if __name__ == "__main__":
-    opc = o3d.read_point_cloud("../../data/objects/apc/genuine_joe_stir_sticks.pcd")
+    opc = o3d.io.read_point_cloud("../../data/objects/apc/genuine_joe_stir_sticks.pcd")
     print(opc)
     o3d.visualization.draw_geometries([opc])
     kpc = open3d_convert.from_open3d(opc)
