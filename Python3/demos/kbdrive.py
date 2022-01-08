2f63b97295a76ac3b607b32d2f26665bbba02146
Kris Hauser <kkhauser@illinois.edu>
2022-01-08 17:32:28 -0500

    Using RIL

--- Python3/demos/kbdrive.py
+++ Python3/demos/kbdrive.py
@@ -2,12 +2,18 @@
 
 import sys
 import klampt
-from klampt.vis.glrobotprogram import GLSimulationPlugin
+from klampt.vis.glinterface import GLPluginInterface
 from klampt import vis
 from klampt.math import vectorops
+from klampt.control.robotinterfaceutils import StepContext,RobotInterfaceCompleter,make_from_file
+from klampt.control.interop import RobotInterfacetoVis
+from klampt.control.simrobotinterface import *
+import time
 
 #FOR DEFAULT JOINT-BY-JOINT KEYMAP: set keymap=None
 keymap = None
+
+SPEED = 1
 
 #FOR CUSTOM KEYMAPS: set up keymap to define how keys map to velocities.
 #keymap is a map from key name to (robot index,velocity vector) pairs.
@@ -15,24 +21,16 @@ keymap = None
 #'left','up','down','right', 'home', 'insert', 'end', and the function keys 'f1',...,'f12'.
 #keymap = {'up':(0,[0,1]),'down':(0,[0,-1]),'left':(0,[-1,0]),'right':(0,[1,0])}
 
-def build_default_keymap(world):
+def build_default_keymap(robot):
     """builds a default keymape: 1234567890 increases values of DOFs 1-10
     of robot 0.  qwertyuiop decreases values."""
-    if world.numRobots() == 0:
-        return {}
-    robot = world.robot(0)
     up = '1234567890'
     down = 'qwertyuiop'
     res = {}
     for i in range(min(robot.numDrivers(),10)):
         #up velocity
-        vel = [0]*robot.numLinks()
-        if robot.driver(i).getType() == 'normal':
-            vel[robot.driver(i).getAffectedLink()] = 1
-        else:
-            #skip it
-            #links = robot.driver(i).getAffectedLinks();
-            continue
+        vel = [0]*robot.numDrivers()
+        vel[i] = SPEED
         res[up[i]] = (0,vel)
         #down velocity
         vel = vectorops.mul(vel,-1)
@@ -41,47 +39,40 @@ def build_default_keymap(world):
 
 
 
-class MyGLViewer(GLSimulationPlugin):
-    def __init__(self,world):
+class MyGLViewer(GLPluginInterface):
+    def __init__(self,interface):
         global keymap
-        GLSimulationPlugin.__init__(self,world)
-        self.world = world
+        self.interface = interface
+        self.robot_model = interface.klamptModel()
+        GLPluginInterface.__init__(self)
+        self.world = interface._worldModel
         if keymap == None:
-            keymap = build_default_keymap(world)
+            keymap = build_default_keymap(self.robot_model)
         self.keymap = keymap
         self.current_velocities = {}
+        self.visplugin = RobotInterfacetoVis(self.interface)
+        self.wait = 0
         #Put your initialization code here
-
-    def control_loop(self):
+        
+    def idle(self):
+        t0 = time.time()
+        self.wait += 1
+        
         #Calculate the desired velocity for each robot by adding up all
         #commands
-        rvels = [[0]*self.world.robot(r).numLinks() for r in range(self.world.numRobots())]
+        rvels = [[0]*self.world.robot(r).numDrivers() for r in range(self.world.numRobots())]
         for (c,(r,v)) in self.current_velocities.items():
             rvels[r] = vectorops.add(rvels[r],v)
-        #print rvels
+        #print(rvels)
         #send to the robot(s)
-        for r in range(self.world.numRobots()):
-            robotController = self.sim.controller(r)
-            qdes = robotController.getCommandedConfig()
-            qdes = vectorops.madd(qdes,rvels[r],self.dt)
-            #clamp to joint limits
-            (qmin,qmax) = self.world.robot(r).getJointLimits()
-            for i in range(len(qdes)):
-                qdes[i] = min(qmax[i],max(qdes[i],qmin[i]))
-                if qdes[i] == qmax[i] or qdes[i] == qmin[i]:
-                    rvels[r][i] = 0
-            robotController.setPIDCommand(qdes,rvels[r])
-        return
-
-    def mousefunc(self,button,state,x,y):
-        #Put your mouse handler here
-        #the current example prints out the list of objects clicked whenever
-        #you right click
-        if button==2:
-            if state==0:
-                print([o.getName() for o in self.click_world(x,y)])
-                return
-        GLSimulationPlugin.mousefunc(self,button,state,x,y)
+        with StepContext(self.interface):
+            if self.wait > 1:
+                ttl = 0.1
+                self.interface.setVelocity(rvels[0],ttl)
+            self.world.robot(0).setConfig(self.interface.configToKlampt(self.interface.sensedPosition()))
+            self.visplugin.update()
+        t1 = time.time()
+        return True
 
     def print_help(self):
         self.window.program.print_help()
@@ -97,7 +88,7 @@ class MyGLViewer(GLSimulationPlugin):
             self.print_help()
             return True
         else:
-            return GLSimulationPlugin.keyboardfunc(self,c,x,y)
+            return GLPluginInterface.keyboardfunc(self,c,x,y)
         self.refresh()
 
     def keyboardupfunc(self,c,x,y):
@@ -107,17 +98,42 @@ class MyGLViewer(GLSimulationPlugin):
 
 
 if __name__ == "__main__":
-    print("kbdrive.py: This example demonstrates how to drive a robot using keyboard input")
+    print("kbdrive_RIL.py: This example demonstrates how to drive a robot")
+    print("(via Robot Interface Layer) using keyboard input")
     if len(sys.argv)<=1:
-        print("USAGE: kbdrive.py [world_file]")
+        print("USAGE: kbdrive_ril.py [world_file(s)] [controller_file]")
         exit()
-    world = klampt.WorldModel()
-    for fn in sys.argv[1:]:
-        res = world.readFile(fn)
-        if not res:
-            raise RuntimeError("Unable to load model "+fn)
 
-    viewer = MyGLViewer(world)
+    world = klampt.WorldModel()
+    interface_fn = None
+    for fn in sys.argv[1:]:
+        if fn.endswith('.py') or fn.endswith('.pyc'):
+            interface_fn = fn
+        else:
+            res = world.readFile(fn)
+            if not res:
+                raise RuntimeError("Unable to load model "+fn)
+    if world.numRobots() == 0:
+        print("No robots loaded")
+        exit(1)
+    #initialize controller or simulated controller by default
+    if interface_fn is None:    
+        sim = klampt.Simulator(world)
+        interface = RobotInterfaceCompleter(SimFullControlInterface(sim.controller(0),sim))
+    else:
+        try:
+            interface = make_from_file(interface_fn,world.robot(0))
+        except Exception:
+            print("Quitting...")
+            sys.exit(1)
+        if not interface.properties.get('complete',False):
+            interface = RobotInterfaceCompleter(interface)   #wrap the interface with a software emulator
+    interface._klamptModel = world.robot(0)
+    interface._worldModel = world
+    if not interface.initialize():
+        print("Robot interface",interface,"Could not be initialized")
+        exit(1)
+    viewer = MyGLViewer(interface)
 
     print() 
     print("**********************")
@@ -126,4 +142,6 @@ if __name__ == "__main__":
     print("Use 1,2,..,0 to increase the robot's joints and q,w,...,p to reduce them.")
     print("**********************")
     print() 
-    vis.run(viewer)
+    vis.add("world",world)
+    vis.pushPlugin(viewer)
+    vis.run()
---
+++ Python3/demos/pose.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import sys
+import klampt
+from klampt import vis
+from klampt.math import vectorops
+from klampt.model.trajectory import RobotTrajectory
+from klampt.control.robotinterfaceutils import StepContext,RobotInterfaceCompleter,MultiprocessingRobotInterface,make_from_file
+from klampt.control.interop import RobotInterfacetoVis
+from klampt.control.simrobotinterface import *
+from klampt.control.utils import TimedLooper
+import time
+
+
+if __name__ == "__main__":
+    print("pose_RIL.py: This example demonstrates how to pose a robot (via Robot Interface Layer)")
+    if len(sys.argv)<=1:
+        print("USAGE: pose_ril.py [world_file(s)] [controller_file]")
+        print('try "python pose_RIL.py ../../data/tx90cuptable.xml"')
+        exit()
+
+    world = klampt.WorldModel()
+    interface_fn = None
+    for fn in sys.argv[1:]:
+        if fn.endswith('.py') or fn.endswith('.pyc'):
+            interface_fn = fn
+        else:
+            res = world.readFile(fn)
+            if not res:
+                raise RuntimeError("Unable to load model "+fn)
+    if world.numRobots() == 0:
+        print("No robots loaded")
+        exit(1)
+    if interface_fn is None:    
+        sim = klampt.Simulator(world)
+        interface = RobotInterfaceCompleter(SimFullControlInterface(sim.controller(0),sim))
+    else:
+        #create interface from specified file
+        sim = None
+        try:
+            interface = make_from_file(interface_fn,world.robot(0))
+        except Exception:
+            print("Quitting...")
+            sys.exit(1)
+        if not interface.properties.get('complete',False):
+            interface = RobotInterfaceCompleter(interface)   #wrap the interface with a software emulator
+    interface._klamptModel = world.robot(0)
+    interface._worldModel = world
+    if not interface.initialize():
+        print("Robot interface",interface,"Could not be initialized")
+        exit(1)
+
+    interface.startStep()
+    cqsns = interface.sensedPosition()
+    world.robot(0).setConfig(interface.configToKlampt(cqsns))
+    interface.endStep()
+
+    target = None
+    trajectory = None
+    def do_move():
+        global target
+        target = vis.getItemConfig("ghost")
+    vis.add("world",world)
+    vis.add("ghost",world.robot(0).getConfig())
+    vis.hide(vis.getItemName(world.robot(0)))
+    vis.edit("ghost")
+    vis.addAction(do_move,"Move to posed config",' ')
+    visplugin = RobotInterfacetoVis(interface)
+    vis.show()
+    dt = 1.0/interface.controlRate()
+    looper = TimedLooper(dt)   #when placed as the while loop guard, looper will try to keep the loop running @ 1/dt Hz
+    iters = 0
+    while vis.shown() and looper: 
+        iters += 1
+        #grab the editor's configuration and send to the robot(s)
+        vis.lock()
+        qdest = vis.getItemConfig("ghost")
+        vis.unlock()
+        with StepContext(interface):
+            cqsns = interface.sensedPosition()
+            cqcmd = interface.commandedPosition()
+            #update the trajectory overlay
+            if cqcmd is not None and cqcmd[0] is not None:
+                qcmd = interface.configToKlampt(cqcmd)
+                qmin,qmax = world.robot(0).getJointLimits()
+                for i in range(len(qcmd)):
+                    if qcmd[i] < qmin[i] or qcmd[i] > qmax[i]:
+                        print("Joint",world.robot(0).link(i).getName(),"Out of limits?",qcmd[i],"vs",qmin[i],qmax[i])
+                vis.add("traj",RobotTrajectory(world.robot(0),milestones=[qcmd,qdest]).getLinkTrajectory(world.robot(0).numLinks()-1,0.1))
+            if target is not None:
+                interface.moveToPosition(interface.configFromKlampt(target))
+                target = None
+            visplugin.update()
+        #give visualizer a chance to update even when controller is running fast
+        time.sleep(0.01)
+    interface.close()
+    vis.kill()
