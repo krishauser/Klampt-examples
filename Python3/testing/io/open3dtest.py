29844a37e78c8e227cd6bcab563dc49e4acafbf5
Kris Hauser <hauser.kris@gmail.com>
2025-01-19 17:43:51 -0500

    Added voxel test

--- Python3/testing/io/open3dtest.py
+++ Python3/testing/io/open3dtest.py
@@ -1,14 +1,20 @@
 import open3d as o3d
 from klampt.io import open3d_convert
-from klampt import vis
+from klampt import vis, Geometry3D
 
 if __name__ == "__main__":
-    print("VERY SIMPLE TEST CONVERTING OPEN3D POINT CLOUDS TO KLAMPT AND BACK")
+    print("SIMPLE TEST CONVERTING OPEN3D POINT CLOUDS TO KLAMPT AND BACK")
     opc = o3d.io.read_point_cloud("../../../data/objects/apc/genuine_joe_stir_sticks.pcd")
     print(opc)
     o3d.visualization.draw_geometries([opc])
     kpc = open3d_convert.from_open3d(opc)
     vis.debug(kpc)
-    vis.kill()
     opc2 = open3d_convert.to_open3d(kpc)
     o3d.visualization.draw_geometries([opc2])
+
+    print("SIMPLE TEST CONVERTING OPEN3D VOXEL GRIDS TO KLAMPT AND BACK")
+    ogrid = o3d.geometry.VoxelGrid.create_from_point_cloud(opc,voxel_size=0.01)
+    kgrid = open3d_convert.from_open3d(ogrid)
+    gridgeom = Geometry3D()
+    gridgeom.setOccupancyGrid(kgrid)
+    vis.debug(gridgeom,kpc)
